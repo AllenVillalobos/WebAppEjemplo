f4a673c941c2c99742ce012ba9072d08e608f616
AllenVillalobos <allenvs06@gmail.com>
2025-05-21 21:03:20 -0600

    Semana#2 Actividad#1

---
+++ Paginas/WebForm.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm.aspx.cs" Inherits="WebAppEjemplo.Paginas.WebForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <p>Hola</p>
+            <div>
+                <h2>Formulario para Validar Edad</h2>
+                <br />
+                <br />
+                <label for="txtNombre">Nombre:</label>
+                <asp:TextBox ID="txtNombre" runat="server" required="true"></asp:TextBox>
+                <br />
+                <br />
+                <label for="txtEdad">Edad:</label>
+                <asp:TextBox ID="txtEdad" runat="server" required="true"></asp:TextBox>
+                <br />
+                <asp:Button ID="btnEnviar" runat="server" Text="Enviar" required="true" OnClick="btnEnviar_Click" />
+                <br />
+                <br />
+                <asp:Label ID="lblMensaje" runat="server"></asp:Label>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Paginas/WebForm.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAppEjemplo.Paginas
+{
+    public partial class WebForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnEnviar_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            int edad;
+            if (int.TryParse(txtEdad.Text, out edad))
+            {
+                lblMensaje.Text = "Se Comvirtio la edad";
+                if (edad < 0)
+                {
+                    lblMensaje.Text = "Edad No valida,Ingrese una Edad Positiva";
+                    lblMensaje.ForeColor = System.Drawing.Color.Red;
+                }
+                else
+                {
+                    if (edad < 18)
+                    {
+                        lblMensaje.Text = "No Cumple con la Edad requerida";
+                        lblMensaje.ForeColor = System.Drawing.Color.Red;
+
+                    }
+                    else
+                    {
+                        lblMensaje.Text = "Ya Cumple con la Edad requerida";
+                        lblMensaje.ForeColor = System.Drawing.Color.Green;
+                    }
+                }
+            }
+            else
+            {
+                lblMensaje.Text = "Ingrese Una Edad Valida";
+            }
+            }
+        }
+    }
+}
